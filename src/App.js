0a66ae39be9705a2db43ce8f4bf29d4d880997b3
Ximena Gonzalez <ximena.glez@gmail.com>
2022-04-04 18:47:05 -0500

    xime glez

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A01028604 Xime Glez
         </a>
       </header>
     </div>
